3a16cea8ebba9c065bc97769404601b65f6f6f90
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2020-05-06 02:36:26 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase'
+
+var firebaseConfig = {
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database()
 
 function App() {
+  const [state, setState] = useState({})
+  useEffect(() => {
+    var incummingData = firebase.database().ref()
+    incummingData.on('value', (snap)=> {
+      let data = snap.val()
+      setState({data})
+    
+    })
+  }, [])
+  const add = ()=> { 
+    let count = state.data.count.number || 0
+    count ++ 
+    database.ref('/count').set({number: count })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state.data? 
+        <div>
+         the count is: - 
+         <p>{state.data.count.number}</p>
+          <button onClick={add}>+</button>
+        </div>
+      : 'loading...'}
     </div>
   );
 }
 
 export default App;
+
+
+/*
+read 
+lets make a counter
+add inputs that pushs in data
+and delete
+
+*/
